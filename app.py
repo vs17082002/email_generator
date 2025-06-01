742943687cbdd6e2af795821f0076e0015807df9
vs17082002 <97960208+vs17082002@users.noreply.github.com>
2025-06-01 16:38:28 +0530

    Update app.py

--- app.py
+++ app.py
@@ -54,4 +54,5 @@ def index():
     return render_template("index.html", email_output=email_output)
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
+
